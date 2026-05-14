56e750607b86289d4d9d89a1e830af800df72666
Ishita Dey <ishitadey.222@gmail.com>
2026-05-15 01:58:02 +0530

    Set up cors

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,32 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../auth.form.scss"
-import { useNavigate,Link } from 'react-router'
+import { useNavigate, Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
 
 
-const Login=()=> {
-  const navigate=useNavigate()
-  const handleSubmit=(e)=>{
+const Login = () => {
+
+  const { loading, handleLogin } = useAuth()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+  const handleSubmit = (e) => {
     e.preventDefault()
+    handleLogin({email,password})
+  }
+
+  if (loading){
+    return (<main><h1>Loading...</h1></main>)
   }
   return (
     <div>
       <main>
         <div className='form-container'>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                    <label htmlFor='email'>Email</label>
-                    <input type="email" id="email" name="email" placeholder="Write your email address"/>
-                </div>
-                <div className="input-group">
-                    <label htmlFor='password'>Password</label>
-                    <input type="password" id="password" name="password" placeholder="Give a password "/>
-                </div>
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor='email'>Email</label>
+              <input
+                onChange={(e) => { setEmail(e.target.value) }}
+                type="email" id="email" name="email" placeholder="Write your email address" />
+            </div>
+            <div className="input-group">
+              <label htmlFor='password'>Password</label>
+              <input
+                onChange={(e) => { setPassword(e.target.value) }}
+                type="password" id="password" name="password" placeholder="Give a password " />
+            </div>
 
-                <button className='button  primary-button'>Login</button>
-                
-            </form>
-            <p>Dont have an account?<Link to={"/register"}>Register</Link></p>
+            <button className='button  primary-button'>Login</button>
+
+          </form>
+          <p>Dont have an account?<Link to={"/register"}>Register</Link></p>
         </div>
       </main>
     </div>
