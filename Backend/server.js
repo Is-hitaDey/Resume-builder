034f4ab0effd244414f5099038035878608c47c0
Ishita Dey <ishitadey.222@gmail.com>
2026-05-17 12:13:00 +0530

    Initital setup of ai services

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config()
 const connectToDB= require("./src/config/database")
 const app= require("./src/app")
+const invokeGeminiAi = require("./src/services/ai.service")
 
 connectToDB()
+// invokeGeminiAi()
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
